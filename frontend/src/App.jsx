deec1d35ee10084123f12905e9616928159667ad
mariem1mansour <125161142+mariem1mansour@users.noreply.github.com>
2025-03-05 22:56:03 +0100

    Defining App Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react/react-router-dom";
+import LoginForm from "./pages/Auth/LoginForm";
+
+import SignUpForm from "./pages/Auth/SignUpForm";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Rout />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signUp" element={<SignUpForm />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;

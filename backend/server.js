b2eec9083680ac0950ed9213cb619bb916f7f4d7
mariem1mansour <125161142+mariem1mansour@users.noreply.github.com>
2025-03-15 22:34:10 +0100

    Connecting To DataBase

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-
+const connectDB = require("./config/db");
 const cors = require("cors");
 const path = require("path");
+const { connect } = require("http2");
 const app = express();
 
 //middleware to handle cors
@@ -16,5 +17,6 @@ app.use(
 
 app.use(express.json());
 
+connectDB();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
